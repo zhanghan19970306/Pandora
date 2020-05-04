62ed73d31e817567f4b00a777c74778191c87e47
zhanghan <zhanghan@budiot.com.cn>
2020-05-04 19:45:58 +0800

    add: 添加流量的格式显示

--- src/transform/index.ts
+++ src/transform/index.ts
@@ -17,3 +17,18 @@ export function moneyToThousand(num: number): string {
         .toString()
         .replace(/^-?\d+/g, (m) => m.replace(/(?=(?!\b)(\d{3})+$)/g, ","))
 }
+
+
+/**
+ * 1024 => "1KB"
+ * @param bytes
+ */
+export function bytesToSize(bytes: number): string {
+    if (bytes === 0) return '0 B'
+    const k = 1024
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+    return parseFloat(String((bytes / Math.pow(k, i)))).toFixed(2) + sizes[i]
+}
+
+
